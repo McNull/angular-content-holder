71292bbb1bbcd1e5e0505d5c4ef936f668db259f
Null McNull <nospam@nowhere.com>
2015-10-15 04:23:34 +0200

    dont reset on search param change

--- src/angular-content-holder/angular-content-holder.js
+++ src/angular-content-holder/angular-content-holder.js
@@ -1,127 +1,148 @@
-(function(angular) {
-  
+(function (angular) {
+
   var ch = angular.module('contentHolder', []);
-  
+
   ch.constant('contentHolderConfig', {
-    resetOnLocationChange: true
+    resetOnLocationChange: true,
+    resetOnSearchChange: false
   });
-  
-  ch.factory('contentHolder', function($rootScope, contentHolderConfig) {
-    
+
+  ch.factory('contentHolder', function ($rootScope, contentHolderConfig, $location) {
+
     var views = {};
-    
+
     function resetView() {
       delete this.template;
       delete this.templateUrl;
       delete this.scope;
     }
-    
+
     function getView(id) {
-      
+
       id = id || 'default';
-      
+
       var view = views[id];
-      
-      if(!view) {
+
+      if (!view) {
         view = views[id] = {
           $id: id, reset: resetView
         };
-      } 
-      
+      }
+
       return view;
     }
+
+    function resetViews() {
+      for (var k in views) {
+        var view = views[k];
+        view.reset();
+      }
+    }
+
+    var lastPath = $location.path();
+    var lastSearch = $location.search();
+
+    function needsReset() {
+      var currentPath = $location.path();
+      var currentSearch = $location.search();
+      
+      var doReset = contentHolderConfig.resetOnLocationChange &&
+                    (contentHolderConfig.resetOnSearchChange || lastPath !== currentPath);
+      
+      lastPath = currentPath;
+      lastSearch = currentSearch;
+      
+      return doReset;
+    }
     
-    $rootScope.$on('$locationChangeSuccess', function() {
-      if(contentHolderConfig.resetOnLocationChange) {
-        for(var k in views) {
-          var view = views[k];
-          view.reset();
-        }
+    $rootScope.$on('$locationChangeSuccess', function () {
+      if(needsReset()) {
+        resetViews();
       }
     });
-    
+
     return {
       getView: getView
     };
-    
+
   });
-  
-  ch.directive('contentView', function(contentHolder, $compile) {
-    
+
+  ch.directive('contentView', function (contentHolder, $compile) {
+
     return {
       scope: true,
       restrict: 'ECA',
       terminal: true,
       priority: 400,
-      link: function($scope, $element, $attrs) {
-        var view = $scope.$view = contentHolder.getView($attrs.contentView);  
+      link: function ($scope, $element, $attrs) {
+        var view = $scope.$view = contentHolder.getView($attrs.contentView);
         var lastScope, lastElement;
-        
+
         $scope.$watch('$view.template', update)
         $scope.$watch('$view.templateUrl', update)
         $scope.$watch('$view.scope', update)
-        
+
         $scope.$on('$destroy', clean);
-            
+
         function clean() {
           // console.log('clean', view.$id);
-          if(lastScope) {
+          if (lastScope) {
             lastScope.$destroy();
             lastScope = null;
           }
-          
-          if(lastElement) {
+
+          if (lastElement) {
             lastElement.remove();
             lastElement = null;
           }
         }
-        
+
         function update(newValue, oldValue) {
           // console.log('update', view.$id)
-          if(newValue === oldValue) {
+          if (newValue === oldValue) {
             return;
           }
           clean();
-          
+
           var scope = view.scope;
-          
-          if(!scope) {
+
+          if (!scope) {
             lastScope = scope = $scope.$new();
           }
-          
+
           lastElement = angular.element(view.template);
-          
+
           $compile(lastElement)(scope);
           $element.append(lastElement);
-          
+
         }
-        
-        update(1,2);
-        
+
+        update(1, 2);
+
       }
     };
-    
+
   });
-  
-  ch.directive('contentHolder', function(contentHolder) {
+
+  ch.directive('contentHolder', function (contentHolder) {
     return {
       restrict: 'ECA',
       terminal: true,
       scope: true,
       link: {
-        pre: function($scope, $element, $attrs, $transclude) {
-          
+        pre: function ($scope, $element, $attrs, $transclude) {
+
           var view = contentHolder.getView($attrs.contentHolder);
           view.reset();
-          
-          view.template = $element.html();
+
+          view.template = '<div>' + $element.html() + '</div>';
           view.scope = $scope;
-          
+
           $element.empty();
 
         }
       }
     };
   });
-  
+
 })(angular);
--- src/app/app.js
+++ src/app/app.js
@@ -19,12 +19,39 @@ app.controller('AppController2', function($scope, contentHolder) {
   this.view1.scope = $scope;
 });
 
+app.controller('AppController3', function($scope, $location) {
+  
+  var vm = this;
+  var path = $location.path();
+  
+  vm.links = [
+    path + '?param=search',
+    path + '?param=arguments',
+    path + '?param=should',
+    path + '?param=not',
+    path + '?param=reset',
+    path + '?param=the',
+    path + '?param=content',
+    path + '?param=views'
+  ];
+  
+  $scope.$watch(function() {
+    return $location.search();
+  }, function(value) {
+    vm.search = value;
+  });
+  
+  vm.createdAt = new Date();
+  
+});
+
 app.config(function($routeProvider) {
   
   $routeProvider.when('/template/:id', {
    templateUrl: function(args) {
     return 'app/template-' + args.id + '.ng.html';
-   } 
+   },
+   reloadOnSearch: false 
   });
   
   $routeProvider.otherwise({
